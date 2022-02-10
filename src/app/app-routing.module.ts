86efb1ff7d9ef8c39ec93a89fdde83f0ad3484fa
Raju Tagline <raju.tagline@gmail.com>
2022-02-10 12:07:26 +0530

    routing done for forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:TemplateFormComponent
+  },
+  {
+    path:'reactive-form',
+    component:ReactiveFormComponent
+  },
+  {
+    path:'template-form',
+    component:TemplateFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
