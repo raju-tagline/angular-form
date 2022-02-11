a207fa57555976fb8e28abc7f1a00635d7692891
Raju Tagline <raju.tagline@gmail.com>
2022-02-11 10:57:04 +0530

    validation completed for white space and forms hobby submitted success

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  EmailValidator,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'reactive-form',
@@ -8,11 +17,8 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 })
 export class ReactiveFormComponent implements OnInit {
   public myRegistrationForm!: FormGroup;
-
   public submit = false;
-
   public formValue = '';
-
   public genders = [
     {
       id: 1,
@@ -23,26 +29,36 @@ export class ReactiveFormComponent implements OnInit {
       genderType: 'Female',
     },
   ];
-  public hobbys: string[] = ['Reading', 'Swimming', 'Cricket', 'Tv'];
+
+  public hobbys: Array<any> = [
+    { name: 'Pear', value: 'pear' },
+    { name: 'Plum', value: 'plum' },
+    { name: 'Kiwi', value: 'kiwi' },
+    { name: 'Apple', value: 'apple' },
+    { name: 'Lime', value: 'lime' },
+    { name: 'Other', value: 'Other' },
+  ];
+
+  // public hobbys: string[] = ['Reading', 'Swimming', 'Cricket', 'Tv'];
 
   // constructor() {}
 
   ngOnInit(): void {
     this.myRegistrationForm = new FormGroup({
-      name: new FormControl('null', [
+      name: new FormControl('Test-name', [
         Validators.required,
-        Validators.minLength(3)
+        Validators.minLength(3),
       ]),
-      email: new FormControl('null@test', [
+      email: new FormControl('test@test.test', [
         Validators.required,
-        Validators.email
+        Validators.email,
+        this.noWhiteSpace
       ]),
       gender: new FormControl('Male', Validators.required),
-      hobby: new FormControl(null, Validators.required),
       country: new FormControl('India', Validators.required),
       state: new FormControl('Gujarat', Validators.required),
       city: new FormControl('Surat', Validators.required),
-      checkArray: new FormArray([], [Validators.required])
+      checkArray: new FormArray([], [Validators.required]),
     });
   }
 
@@ -54,7 +70,7 @@ export class ReactiveFormComponent implements OnInit {
       this.myRegistrationForm.value
     );
 
-    const { city, country, email, gender, hobbys, name, state } =
+    const { city, country, email, gender, checkArray, name, state } =
       this.myRegistrationForm.value;
 
     // this.name = name;
@@ -65,43 +81,35 @@ export class ReactiveFormComponent implements OnInit {
     // this.city = city;
 
     this.formValue = this.myRegistrationForm.value;
-    console.log('this.myRegistrationForm :>> ', this.myRegistrationForm.value);
+
+    // console.log('this.myRegistrationForm :>> ', this.myRegistrationForm.value);
     this.myRegistrationForm.reset();
   }
 
-  public Data: Array<any> = [
-    { name: 'Pear', value: 'pear' },
-    { name: 'Plum', value: 'plum' },
-    { name: 'Kiwi', value: 'kiwi' },
-    { name: 'Apple', value: 'apple' },
-    { name: 'Lime', value: 'lime' },
-    { name: 'Other', value: 'Other' },
-  ];
-  // constructor(private fb: FormBuilder) {
-  //   this.myRegistrationForm = this.fb.group({
-      
-  //   })
-  // }
+  //checkbox validation
   onCheckboxChange(e: any) {
-    const checkArray: FormArray = this.myRegistrationForm.get('checkArray') as FormArray;
+    const checkArray: FormArray = this.myRegistrationForm.get(
+      'checkArray'
+    ) as FormArray;
     if (e.target.checked) {
       checkArray.push(new FormControl(e.target.value));
     } else {
       let i: number = 0;
       checkArray.controls.forEach((item) => {
-          if (item.value == e.target.value) {
-            checkArray.removeAt(i);
-            return;
-          }
-          i++;
-        });
+        if (item.value == e.target.value) {
+          checkArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
     }
   }
-  submitForm() {
-    console.log(this.myRegistrationForm.value)
-  }
 
-  // public changeGender(e:any){
-  //   console.log(e.target.value);
-  // }
+  //no white spaces
+  public noWhiteSpace(control: AbstractControl) : ValidationErrors | null {
+    if((control.value as string).indexOf(' ') >= 0){
+        return {noWhiteSpace: true}
+    }
+    return null;
+}
 }
