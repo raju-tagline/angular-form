269b2753c5da071647827a0269e91365062759a4
Raju Tagline <raju.tagline@gmail.com>
2022-02-14 15:20:45 +0530

    json pipe completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ export class AppComponent {
   title = 'forms';
   public currentTime = new Date();
   details = `The built-in pipes are those which are already given to us by the Angular and are ready to use. `;
+  object: Object = {
+    name: 'Mr. abcd',
+    username: 'qux_asd',
+    address: { country: 'IND', State:'Gujarat',city:'surat'  },
+  };
 }
