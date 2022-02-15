e94f3c55ac5da9768f7348921cbad6962f75631c
Raju Tagline <raju.tagline@gmail.com>
2022-02-15 11:33:30 +0530

    custom filter pipe completed for multiple fields

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,14 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
 })
 export class FilterPipe implements PipeTransform {
-
-  transform(value: any, searchTerm:any): any {
-    return value.filter( (search:any) => {
-      return search.product.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
-    })
+  transform(value: any, searchTerm: any): any {
+    return value.filter((search: any) => {
+      return (
+        search.product.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1 ||
+        search.price.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1 ||
+        search.available.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1
+      );
+    });
   }
-
 }
