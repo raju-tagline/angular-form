fe1910ab53f92684006e60c6abcd1fbfb5f0d1b2
Raju Tagline <raju.tagline@gmail.com>
2022-02-16 12:30:04 +0530

    interface added inservice

--- src/app/http-data.service.ts
+++ src/app/http-data.service.ts
@@ -1,30 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { UserData } from './interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpDataService {
   public url: string = 'https://jsonplaceholder.typicode.com/todos';
+  public userDetail$ = new BehaviorSubject([]);
+
+  public userD$() {
+    console.log('Behaviour subject :>> ');
+    return this.userDetail$.asObservable();
+  }
 
   constructor(public http: HttpClient) {}
 
-  public getData(): Observable<any> {
-    return this.http.get(this.url);
+  //Read data for http
+  public getData(): Observable<UserData[]> {
+    return this.http.get<UserData[]>(this.url);
   }
 
-  public postUsers(data: any): Observable<any> {
-    return this.http.post(this.url, data);
+  //Create data in http
+  public postUsers(user: UserData[]): Observable<UserData[]> {
+    return this.http.post<UserData[]>(this.url, user);
   }
 
-  public updateUser(userId: number, user: any): Observable<any> {
+  //Update data of http
+  public updateUser(userId: number, user: UserData[]): Observable<UserData[]> {
     const putUrl = this.url + '/' + userId;
-    return this.http.put(putUrl, user);
+    return this.http.put<UserData[]>(putUrl, user);
   }
 
-  public deleteUser(userId:number): Observable<{}> {
+  //Delete data of http
+  public deleteUser(userId: number): Observable<UserData[]> {
     const deleteUrl = this.url + '/' + userId;
-    return this.http.delete(deleteUrl);
-}  
+    return this.http.delete<UserData[]>(deleteUrl);
+  }
 }
